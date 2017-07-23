4fd08099bc7461ab376c6e9614131d898f298942
Victor Hernandez <vikhhr@gmail.com>
2017-07-22 18:04:36 -0600

    Hijacking all requests.

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -3,4 +3,7 @@ self.addEventListener('fetch', function(event) {
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
   console.log(event.request);
+  event.respondWith(new Response('Hello 2 <b class="a-winner-is-me">world</b>', {
+	  	headers: {'Content-Type': 'text/html'}
+	  }));
 });
